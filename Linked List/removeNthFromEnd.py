72cb7114797cb9bc590c559b4421a15770d788c1
SamChichester <sam@samuels-mbp.mynetworksettings.com>
2024-08-10 19:31:47 -0400

    Completed Linked List removeNthFromEnd

---
+++ Linked List/removeNthFromEnd.py
@@ -0,0 +1,33 @@
+"""
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+"""
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        first = head
+        second = dummy
+
+        for _ in range(n):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+        return dummy.next
+
+
+if __name__ == "__main__":
+    solution = Solution()
